5976dd2a7343fbc61a85c3d846e2b4f0784717c2
RodriiTepatzi <72983645+RodriiTepatzi@users.noreply.github.com>
2021-09-02 23:34:41 -0500

    Creating the project and adding a margin setter to use in XAML and a CommandHandler (still developing)

---
+++ Models/UserModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kappa.Models
+{
+    class UserModel
+    {
+        /// <summary>
+        /// Gets or sets the id of the user
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the user
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the country of the user
+        /// </summary>
+        public string Country { get; set; }
+
+        /// <summary>
+        /// Gets or sets the email of the user
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets the telephone number country of the user
+        /// </summary>
+        public string TNumber { get; set; }
+
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Kappa.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Kappa.ViewModels
+{
+    class MainWindowViewModel
+    {
+        /*private ICommand _close;
+        public ICommand Close
+        {
+            get
+            {
+                return _close ?? (_close = new CommandHandler(CloseEvent));
+            }
+        }*/
+
+        public RoutedUICommand CloseCommand { get; set; } = new RoutedUICommand("Close", "CloseCommand", typeof(RoutedEventHandler));
+        private void CloseEvent(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
